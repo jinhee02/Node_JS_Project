7e5a7517891215e12606b204cb149874900df235
jinhee02 <jinhee02@g.shingu.ac.kr>
2023-04-10 15:18:04 +0900

    레지스터 통신 까지

--- ExpressProject/Assets/Scripts/RankMain.cs
+++ ExpressProject/Assets/Scripts/RankMain.cs
@@ -1,8 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
-using UnityEngine.Networking;
+using UnityEngine.UI;           //유니티 UI 접근
+using UnityEngine.Networking;   //유니티 Networking 사용
 using System.Text;
 
 public class RankMain : MonoBehaviour
@@ -13,25 +13,54 @@ public class RankMain : MonoBehaviour
     public string idUri;
     public string postUri;
     public string id;
+    public string pw;                       //추가
     public int score;
 
-    public Button BtnGetTop3;
-    public Button BtnGetld;
-    public Button BtnPost;
+    public string registerIDUri;            //추가
 
+    public Button BtnGetTop3;
+    public Button BtnGetId;
+    public Button BtnPost;
+    public Button BtnRegisterID;            //추가
+
+    Dictionary<string, int> scoreDic = new Dictionary<string, int>();
     void Start()
     {
-        this.BtnGetld.onClick.AddListener(() =>
+        this.BtnRegisterID.onClick.AddListener(() =>
         {
-            var url = string.Format("{0}:{1}/{2}", host, postUri, idUri + "/" + id);
+            var url = string.Format("{0}:{1}/{2}", host, port, registerIDUri);
             Debug.Log(url);
 
-            StartCoroutine(this.Getld(url, (raw) =>
+            var req = new Protocols.Packets.req_registerid();
+            req.cmd = 1000;
+            req.userid = new Protocols.Packets.userid();
+            req.userid.id = id;
+            req.userid.pw = pw;
+
+            var Json = JsonUtility.ToJson(req);                         //Json 화 시켜줌 
+
+            StartCoroutine(this.PostRegister(url, Json ,(raw) =>            
             {
+                
+                var res = JsonUtility.FromJson<Protocols.Packets.res_registerid>(raw);
+                Debug.LogFormat("{0}, {1}", res.cmd, res.message);
+
+            }));
+        });
+
+        this.BtnGetId.onClick.AddListener(() =>
+        {
+            var url = string.Format("{0}:{1}/{2}", host, port, idUri + "/" + id);
+            Debug.Log(url);
+
+            StartCoroutine(this.GetId(url, (raw) =>
+            {              
+                //var res = JsonConvert.DeserializeObject<Protocols.Packets.res_scores_id>(raw);
                 var res = JsonUtility.FromJson<Protocols.Packets.res_scores_id>(raw);
                 Debug.LogFormat("{0}, {1}", res.result.id, res.result.score);
 
             }));
+
         });
 
         this.BtnGetTop3.onClick.AddListener(() =>
@@ -39,13 +68,17 @@ public class RankMain : MonoBehaviour
             var url = string.Format("{0}:{1}/{2}", host, port, top3Uri);
             Debug.Log(url);
 
-            StartCoroutine(this.Getld(url, (raw) =>
+            StartCoroutine(this.GetTop3(url, (raw) =>
             {
-                var res = JsonUtility.FromJson<Protocols.Packets.res_scores_top3>(raw);
-                foreach (var user in res.resilt)
+                //var res = JsonConvert.DeserializeObject<Protocols.Packets.res_scores_top3>(raw);
+                Protocols.Packets.res_scores_top3 res = JsonUtility.FromJson<Protocols.Packets.res_scores_top3>(raw);
+
+                Debug.LogFormat("{0}, {1}", res.cmd, res.result.Length);
+                foreach (var user in res.result)
                 {
-                    Debug.LogFormat("{0}, {1}", user.id, user.score);
+                    Debug.LogFormat("{0} : {1}", user.id, user.score);
                 }
+
             }));
         });
 
@@ -58,50 +91,56 @@ public class RankMain : MonoBehaviour
             req.cmd = 1000;
             req.id = id;
             req.score = score;
+            //var json = JsonConvert.SerializeObject(req);
             var json = JsonUtility.ToJson(req);
             Debug.Log(json);
 
             StartCoroutine(this.PostScore(url, json, (raw) =>
-            {
-                Protocols.Packets.res_scores res = JsonUtility.FromJson<Protocols.Packets.res_scores>(raw);
+            {              
+                //Protocols.Packets.res_scores res = JsonConvert.DeserializeObject<Protocols.Packets.res_scores>(raw);
+                Protocols.Packets.res_scores res = JsonUtility.FromJson<Protocols.Packets.res_scores_id>(raw);
                 Debug.LogFormat("{0}, {1}", res.cmd, res.message);
             }));
+
         });
     }
 
 
-    private IEnumerator PostScore(string url, string json, System.Action<string>callback)
+    private IEnumerator PostScore(string url, string json, System.Action<string> callback)
     {
         var webRequest = new UnityWebRequest(url, "POST");
-        var bodyRaw = Encoding.UTF8.GetBytes(json);
+        var bodyRaw = Encoding.UTF8.GetBytes(json);                         //직렬화 (문자열 -> 바이트 배열)
 
+       
         webRequest.uploadHandler = new UploadHandlerRaw(bodyRaw);
         webRequest.downloadHandler = new DownloadHandlerBuffer();
         webRequest.SetRequestHeader("Content-Type", "application/json");
 
-        yield return webRequest.SendWebRequest();
+        yield return webRequest.SendWebRequest();                           //Node.js 로 보냄
 
-        if(webRequest.result == UnityWebRequest.Result.ConnectionError ||
-            webRequest.result ==UnityWebRequest.Result.ProtocolError)
+        if (webRequest.result == UnityWebRequest.Result.ConnectionError ||                  //각종 네트워크 에러 사항 채킹
+            webRequest.result == UnityWebRequest.Result.ProtocolError)
         {
             Debug.Log("네트워크 환경이 좋지 않음");
         }
         else
         {
-            Debug.LogFormat("{0\n{1}\n[2]\n", webRequest.responseCode, webRequest.downloadHandler.data, webRequest.downloadHandler.text);
+            Debug.LogFormat("{0}/{1}/{2}/", webRequest.responseCode, webRequest.downloadHandler.data, webRequest.downloadHandler.text);
             callback(webRequest.downloadHandler.text);
         }
+
+        webRequest.uploadHandler.Dispose();
     }
 
-    private IEnumerator Getld(string url, System.Action<string>callback)
+    private IEnumerator GetId(string url, System.Action<string> callback)
     {
         var webRequest = UnityWebRequest.Get(url);
         yield return webRequest.SendWebRequest();
 
         Debug.Log("----->" + webRequest.downloadHandler.text);
 
-        if(webRequest.result == UnityWebRequest.Result.ConnectionError ||
-            webRequest.result == UnityWebRequest.Result.ProtocolError)
+        if (webRequest.result == UnityWebRequest.Result.ConnectionError ||                  //각종 네트워크 에러 사항 채킹
+           webRequest.result == UnityWebRequest.Result.ProtocolError)
         {
             Debug.Log("네트워크 환경이 좋지 않음");
         }
@@ -110,13 +149,14 @@ public class RankMain : MonoBehaviour
             callback(webRequest.downloadHandler.text);
         }
     }
+
     private IEnumerator GetTop3(string url, System.Action<string> callback)
     {
         var webRequest = UnityWebRequest.Get(url);
         yield return webRequest.SendWebRequest();
 
-        if (webRequest.result == UnityWebRequest.Result.ConnectionError ||
-            webRequest.result == UnityWebRequest.Result.ProtocolError)
+        if (webRequest.result == UnityWebRequest.Result.ConnectionError ||                  //각종 네트워크 에러 사항 채킹
+           webRequest.result == UnityWebRequest.Result.ProtocolError)
         {
             Debug.Log("네트워크 환경이 좋지 않음");
         }
@@ -125,4 +165,33 @@ public class RankMain : MonoBehaviour
             callback(webRequest.downloadHandler.text);
         }
     }
+
+    private IEnumerator PostRegister(string url, string json, System.Action<string> callback)
+    {
+        var webRequest = new UnityWebRequest(url, "POST");
+        var bodyRaw = Encoding.UTF8.GetBytes(json);                         //직렬화 (문자열 -> 바이트 배열)
+
+
+        webRequest.uploadHandler = new UploadHandlerRaw(bodyRaw);
+        webRequest.downloadHandler = new DownloadHandlerBuffer();
+        webRequest.SetRequestHeader("Content-Type", "application/json");
+
+        yield return webRequest.SendWebRequest();                           //Node.js 로 보냄
+
+        if (webRequest.result == UnityWebRequest.Result.ConnectionError ||                  //각종 네트워크 에러 사항 채킹
+            webRequest.result == UnityWebRequest.Result.ProtocolError)
+        {
+            Debug.Log("네트워크 환경이 좋지 않음");
+        }
+        else
+        {
+            Debug.LogFormat("{0}/{1}/{2}/", webRequest.responseCode, webRequest.downloadHandler.data, webRequest.downloadHandler.text);
+            callback(webRequest.downloadHandler.text);
+        }
+
+        webRequest.uploadHandler.Dispose();
+    }
+
+
+
 }
--- score.js
+++ score.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const app = express();
 
-let users = [];
+let users = [];                                     //App.js 에서 users 배열 추가 
+let usersid = [];                                   //user id 배열 추가 
 
 app.use(express.json());
 
@@ -10,70 +11,67 @@ app.get('/', (req, res)=>{
 });
 
 app.get('/scores/:id', (req, res)=>{
-    console.log('id :' + req.params.id);
-
-    let user = users.find(x=>x.id == req.params.id);
-
-    if(user === undefined)
-    {
+    console.log('id :' + req.params.id);                    //Unity에서 req에 id를 무었을 보냈는지 확인 
+    
+    let user = users.find(x=>x.id == req.params.id);        //배열 값에 유저가 있나 확인
+   
+    if(user === undefined)                                  //if(user == null) => if(user === undefined)    
+    { 
         res.send({
             cmd : 1103,
-            message : '잘못된 id 입니다.'
+            message : '잘못된 id 입니다. '
         });
     }
-    else
+    else 
     {
         res.send({
             cmd : 1102,
             message : '',
-            result : user
-        });    
-    }
+            result : user                                     //찾은 유저를 Unity에 전송 
+        });
+    }   
 });
 
 app.get('/scores/top3', (req, res)=>{
-    let result = users.sort(function (a,b) {
+    let result = users.sort(function (a,b) {                    //Users를 sort
         return b.score - a.score;
     });
 
-    result = result.slice(0,3);
-
+    result = result.slice(0,3);                                 // 0 ~ (n - 1)  1~3등을 보여준다. 
+    
     res.send({
         cmd : 1101,
         message : '',
-        result
-    });   
-
+        result                                                  //찾은 유저를 Unity에 전송 
+    });
 
 });
 
 app.post('/scores', (req, res)=>{
-    const {id, score} = req.body;
-
+    const {id ,score} = req.body;
     console.log(req.body);
-
     let result = {
         cmd : -1,
         messasge : ''
     };
 
-    let user = users.find(x=>x.id == id);
+    let user = users.find(x=>x.id == id); 
 
     if(user === undefined)
     {
         users.push({id, score});
         result.cmd = 1001;
-        result.messasge = '점수가 신규 등록 되었습니다.'
+        result.message = '점수가 신규 등록 되었습니다.'
     }
     else
     {
-        console.log(score, id, user.score);
+        console.log(score, id , user.score);
 
         if(score > user.score)
         {
             user.score = score;
             result.cmd = 1002;
-            result.messasge = '점수가 갱신 되었습니다.'
+            result.message = '점수가 갱신 되었습니다. '
         }
         else
         {
@@ -82,7 +80,36 @@ app.post('/scores', (req, res)=>{
     }
     console.log(users);
     res.send(result);
-})
+
+});
+
+// let usersid = []         위쪽에 선언
+app.post('/register', (req, res)=>{
+   
+    console.log(req.body.userid.id);                    //req Unity3D에서 쏜 값을 가져온다. 
+    let id = req.body.userid.id;
+    let password = req.body.userid. password;
+    let result = {
+        cmd : -1,
+        message : ''
+    }
+    let userid = usersid.find(x=>x.id == id);
+
+    if(userid === undefined)
+    {
+        usersid.push({id, password});
+        result.cmd = 1101;
+        result.message = '신규 아이디가 등록 되었습니다. '
+    }
+    else
+    {
+        result.cmd = 1102;
+        result.message = '이미 존재하는 아이디 입니다. '      
+    }   
+    res.send(result);
+});
+
+
 
 app.listen(3030, ()=> {
     console.log('server is running at 3030 port');
